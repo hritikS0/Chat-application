d64b71831a478a2b1ce1565e8c1eaf1ff3834593
hritikS0 <168188588+hritikS0@users.noreply.github.com>
2025-04-28 21:01:23 +0530

    Update chatRoom.js

--- js/chatRoom.js
+++ js/chatRoom.js
@@ -3,16 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, set, push, update, onDisconnect, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAgI4VQ0yUlcJOm_X7OHkeQ9L3BVlW1wyc",
-    authDomain: "app-application-f73b9.firebaseapp.com",
-    databaseURL: "https://app-application-f73b9-default-rtdb.firebaseio.com",
-    projectId: "app-application-f73b9",
-    storageBucket: "app-application-f73b9.appspot.com",
-    messagingSenderId: "127997162999",
-    appId: "1:127997162999:web:69da92f7a4c8b02631aaf2",
-    measurementId: "G-VEWJTWJ710"
-};
+//Paste your firebase api
+        const firebaseConfig = {
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: "",
+            measurementId: ""
+}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
