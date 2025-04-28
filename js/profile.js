c43378bc75f1f05100a5134e9cff47c068fc404d
hritikS0 <168188588+hritikS0@users.noreply.github.com>
2025-04-28 21:01:02 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -2,17 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
         import { getDatabase, ref, update, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
         import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
         import { avatars } from "/js/avatars.js";
-
+//Paste your firebase api
         const firebaseConfig = {
-            apiKey: "AIzaSyAgI4VQ0yUlcJOm_X7OHkeQ9L3BVlW1wyc",
-            authDomain: "app-application-f73b9.firebaseapp.com",
-            databaseURL: "https://app-application-f73b9-default-rtdb.firebaseio.com",
-            projectId: "app-application-f73b9",
-            storageBucket: "app-application-f73b9.appspot.com",
-            messagingSenderId: "127997162999",
-            appId: "1:127997162999:web:69da92f7a4c8b02631aaf2",
-            measurementId: "G-VEWJTWJ710"
-        };
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: "",
+            measurementId: ""
+};
 
         const app = initializeApp(firebaseConfig);
         const db = getDatabase(app);
